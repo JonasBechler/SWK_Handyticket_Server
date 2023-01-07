215eaf670f790f0254db6bacdcdd0a97463f0369
Jonas Bechler <jonasbechler@gmail.com>
2023-01-07 15:32:27 +0100

    global fusionauth config

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
 const path = require('path');
 
 const config = require('../../config.json')
-const fusionAuth_config = require('../fusionauth_config.json')
 
 config.port = config.handyticket.port
 config.port_react = config.handyticket.port_react
 config.session_secret = config.handyticket.session_secret
-config.fusionauth = fusionAuth_config
+config.fusionauth = config.handyticket.fusionauth
 
 const frontend_dir = path.join(__dirname, "..", "..", "SWK_Handyticket_React", "build")
 const userDataPath = path.join(__dirname, "..", "data", "accounts.json")
