c245bc2aa9beabb9e82a6c76450d04e02ee7ed73
JonasBechler <jonasbechler@gmail.com>
2023-01-16 20:16:45 +0100

    update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ config.port_react = config.handyticket.port_react
 config.session_secret = config.handyticket.session_secret
 config.fusionauth = config.handyticket.fusionauth
 
-const frontend_dir = path.join(__dirname, "..", "..", "SWK_Handyticket_React", "build")
+const frontend_dir = path.join(__dirname, "..", "SWK_Handyticket_React", "build")
 const userDataPath = path.join(__dirname, "..", "data", "accounts.json")
 
 var app = require('./SWK_Server_Template/src/main')(config, userDataPath, frontend_dir)
